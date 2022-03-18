4a91266803edaecb8e0ff1a72011a3a5f0f0a6fd
한승엽 <tmdduq525@naver.com>
2022-03-18 23:06:05 +0900

    feat: LoginPage Component
    LoginPage -> Page that user can signin
    Header -> Header on LoginPage
    LoginForm -> Body LoginForm on LoginPage

---
+++ src/Login/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header>
+          <div className="header">
+            <h1>Login</h1>
+          </div>
+        </header>
+    );
+}
+
+export default Header;
