615c090e9da542d20569e772b2f0f849a31cb45e
한승엽 <tmdduq525@naver.com>
2022-03-19 00:44:55 +0900

    feat: Login Form Design Modify
    Material UI로 변경
    Username(ID), Password(PW)의 Textfield 구현

--- src/Login/LoginPage.js
+++ src/Login/LoginPage.js
@@ -1,14 +1,14 @@
-import Header from './Header'
 import LoginForm from './LoginForm'
 
+import { Container, CssBaseline } from "@mui/material";
+
 function LoginPage() {
-    return (
-    <>
-      <Header/>
-      <LoginForm/>
-      <h3><a href="admin.php">회원가입</a></h3>
-    </>
-    );
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <LoginForm />
+    </Container>
+  );
 }
 
 export default LoginPage;
