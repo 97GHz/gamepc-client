84f6344d4c6921b854ff62278c9c9d88a470c0df
한승엽 <tmdduq525@naver.com>
2022-03-20 02:01:46 +0900

    feat: HomePage MUI Design
    App.js -> Manual Routing
    Home.js -> Main Page
    Timetable.js -> Refactor and Renewal Design

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,26 @@
-import Header from "./Header"
 import Timetable from "./Timetable"
+import { Container, CssBaseline, Typography, Box } from "@mui/material"
 
 function Home() {
+    let isLoggedIn = false;
+    let name = "한승엽";
+
     return (
-        <>
-            <Header />
-            <Timetable />
-        </>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box>
+                <Typography align="center" variant="h4" sx={{ mt: 4 }}>
+                    Game PC Reservation
+                </Typography>
+                {isLoggedIn
+                    ? <Typography align="right" variant="h6">{name}님</Typography>
+                    : <Typography align="right" variant="h6">로그인이 필요합니다.</Typography>
+                }
+                <Typography align="right" variant="h6">오늘 일자 : 2022년 3월 17일</Typography>
+                <Typography align="right" variant="h6">현재 시각 : 12:10:23</Typography>
+            </Box>
+            <Timetable sx={{ mt: 4 }} />
+        </Container>
     );
 }
 
